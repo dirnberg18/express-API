e857046ffc6e460f3ab4847ddd5d4839fc572ad3
Marlene <marlene.dirnberger@edu.fh-joanneum.at>
2020-12-23 21:29:11 +0200

    deploy commit

--- database.js
+++ database.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool
 
 const connection = new Pool({
-  user: 'netuser',
-  host: 'localhost',
-  database: 'netdb',
-  password: 'netpass',
+  user: 'glarnlrpxnbyry',
+  host: 'ec2-54-247-107-109.eu-west-1.compute.amazonaws.com',
+  database: 'dabt8gks5lqsaf',
+  password: '06a54954af2c295e122393dd7f18bb94fa5f3293e503140ee2a5ee922078af36',
   port: 5432,
 })
 module.exports = connection;
